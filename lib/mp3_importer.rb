53515ba6e6b1db1da1504f670db1e204b619cb7c
Sara Nolen <nolensara1@gmail.com>
2021-02-07 22:31:40 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,6 +7,8 @@ class MP3Importer
   end
 
   def files
+    @files = Dir.entries(@path)
+    @files.delete_if{|file| file == "." || file == ".."}
     @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
