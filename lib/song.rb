60ea87352e4d3fb870ece9af6aac78b971bdc810
Sara Nolen <nolensara1@gmail.com>
2021-02-07 23:01:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
     song = self.new(split_name[1])
     artist = Artist.find_or_create_by_name(split_name[0])
     song.artist = artist
-    @artist.song = song
+    
     song
   end
 
