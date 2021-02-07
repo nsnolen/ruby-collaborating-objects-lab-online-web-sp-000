fb3f92aa8bfd6895c1fdd9238e2af8704156105d
Sara Nolen <nolensara1@gmail.com>
2021-02-07 22:23:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song, artist = filename.split(" - ")
+    artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = Artist.find_or_create_by_name(artist)
     new_song.save
