dac6795b59690e373b1f84bf433a9474f0bb2cdc
Sara Nolen <nolensara1@gmail.com>
2021-02-08 00:31:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,14 +24,9 @@ class Artist
 
   end
 
-  def self.find_or_create_by_name(name)
-    self.all.detect do |artist|
-      if artist.name == name
-        name
-      else
-        self.new(name)
+    def self.find_or_create_by_name(name)
+      self.all.detect{|artist| artist.name == name} || self.new(name)
       end
-    end
   end
 
   def self.find(name)
